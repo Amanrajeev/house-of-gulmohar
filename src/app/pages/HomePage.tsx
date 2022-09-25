3c340681752a5d022ca1e236f3cac5dd925a7544
aman <aman@qburst.com>
2022-09-25 11:49:50 +0530

    Basic Skelton Created

---
+++ src/app/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../views/Home/HeroSection/HeroSection'
+
+const HomePage = () => {
+  return (
+    <div>
+        <HeroSection/>
+    </div>
+  )
+}
+
+export default HomePage
